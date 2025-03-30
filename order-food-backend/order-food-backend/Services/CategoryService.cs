baa809c368dcf5653787c983e207fa9cbbdc1f33
luizolivas <luizolivas@gmail.com>
2025-03-30 19:31:28 -0300

    Create Category Repository and Service

---
+++ order-food-backend/order-food-backend/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using order_food_backend.Repositories.Interfaces;
+using order_food_backend.Services.Interfaces;
+using OrderFoodLibrary.Entities;
+
+namespace order_food_backend.Services {
+    public class CategoryService : ICategoryService 
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository) {
+            _categoryRepository = categoryRepository;
+        }
+
+        public Task AddCategory(Category category) {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategory(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetCategories() {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategory(Category category) {
+            throw new NotImplementedException();
+        }
+    }
+}
